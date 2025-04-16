0fbd6e1b5a4c1c3e7803edfc999025aea9df88e6
Jans Alzate Morales <c0936855@mylambton.ca>
2025-04-16 13:44:09 -0400

    Python script merging phys-chem with bioactivity data
    This script was used to map and merge the information coming from Tables obtained with RDKit and Tables with biological activity reported in the reference manuscript.

---
+++ mapping-merging_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Load and clean column names
+data1 = pd.read_csv('dG-Tabla1.csv', delimiter=';')
+data1.columns = data1.columns.str.strip()
+
+data2 = pd.read_csv('kinase-ligands_merged_output.csv', delimiter=';')
+data2.columns = data2.columns.str.strip()
+
+# Merge on Ligand (from data1) and ID (from data2)
+merged = data2.merge(data1[['Ligand', 'Exp. dG', 'Pred. dG']], left_on='ID', right_on='Ligand', how='left')
+
+# Optional: drop redundant 'Ligand' column (since we have 'ID' already)
+merged = merged.drop(columns=['Ligand'])
+
+# Save result
+merged.to_csv('updated_data2.csv', index=False)
+
+
+
